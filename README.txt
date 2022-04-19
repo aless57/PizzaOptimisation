583d9f627caf42576543f101a6d9917a60b0b14f
Aless <demangealessi@gmail.com>
2022-04-19 13:24:19 +0200

    Code OK

---
+++ README.txt
@@ -0,0 +1,9 @@
+DEMANGE Alessi & NOEL Victor - Licence 3 Informatique
+
+Projet Pizza Optimisation
+
+Pour trouver la solution à chacun des problèmes, il suffit de lancer le programme qui se nomme "main.py" sans aucun argument.
+Dans le terminal : "python3 main.py"
+
+Le programme se lance sans aucun souci si les ".txt" n'ont pas changé de nom, si vous avez changé les noms des problèmes,
+il faut les mettre à jour dans le tableau du fichier main.py ligne 2.
---
+++ main.py
@@ -0,0 +1,59 @@
+#Nom des fichiers à ouvrir et à traiter
+filesIN=["a_exemple.txt","b_basique.txt","c_grossier.txt","d_difficile.txt","e_elabore.txt"]
+# Boucle sur les problèmes
+for f in filesIN:
+    # list out pour la réécriture
+    listeIngredientIN = {}
+    # Ouverture et lecture dans les fichiers (nom dans 'filesIN')
+    with open(f) as file:
+        #On sépare chaque élement dans le fichier
+        data = file.read().split()
+        #On se place sur la ligne de ce que le client 1 aime
+        next = 1
+        #On boucle sur le nombre de clients
+        for i in range(int(data[0])):
+            #Nombre d'ingredient qu'aime le client i
+            aime = int(data[next])
+            if aime != 0:
+                for i in range(aime):
+                    # On se place sur la ligne de ce que n'aime pas le client i
+                    next +=1
+                    # INGREDIENT AIME : Si l'ingredient est dans la list out alors on incremente de 1 sinon on le rejoute dedans 
+                    if data[next] in listeIngredientIN:
+                        listeIngredientIN[data[next]] += 1
+                    else:
+                        listeIngredientIN[data[next]] = 1
+            # On se place sur la ligne de ce que n'aime pas le client i
+            next+=1
+            # Nombre d'ingredient que n'aime pas le client i
+            aimepas = int(data[next])
+            if aimepas !=0:
+                for i in range(aimepas):
+                    # On se place sur la ligne de ce qu'aime le client i+1
+                    next +=1
+                    # INGREDIENT NON AIME : Si l'ingredient est dans la list out alors on incremente de 1 sinon on le rejoute dedans 
+                    if len(data) != next:
+                        if data[next] in listeIngredientIN:
+                            listeIngredientIN[data[next]] -= 1
+                        else:
+                            listeIngredientIN[data[next]] = 0
+                    else:
+                        break
+            # On se place sur la ligne de ce qu'aime le client i+1
+            next+=1
+    
+    #Variable pour la reecriture du fichier
+    listeIngredientOUT = []
+    # On boucle sur chaque element de la list out
+    for i in listeIngredientIN:
+        # Si l'ingredient est supperieur a 0 alors on l'ajoute dans la reecriture du fichier (<=0 : ingredient detesté de N clients )
+        if listeIngredientIN[i] > 0:
+            listeIngredientOUT.append(i)
+    listeIngredientOUT.insert(0,len(listeIngredientOUT))
+    #Split du nom des ficher pour récupérer la lettre du test
+    fn = f.split("_")
+    #Création du fichier et écriture dedans pour la solution des problèmes
+    file=open("exemple_solution_"+fn[0]+".txt",'w')
+    for tof in listeIngredientOUT:
+        file.write(str(tof)+" ")
+    file.close()
